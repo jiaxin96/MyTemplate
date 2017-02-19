9aaff787679ac2a4fd0f76178de4f845c0464456
jiaxin96 <572673807@qq.com>
2017-02-19 23:11:01 +0800

    minmax

---
+++ AI/minmax.java
@@ -0,0 +1,71 @@
+
+	int max_val(int pb) {
+		int alpha = -INF;
+		int beta = pb;
+
+		if (deep == 0) {
+			return eval();
+		}
+
+		int [] step = new int[2]{startpos, endpos};
+
+		int t;
+
+		for (int i : step) {
+
+
+			t = eval();
+
+			if (t > alpha) {
+				alpha = t;
+			}
+
+
+			if (alpha >= beta) {
+				break;
+			}
+
+		}
+
+
+		alpha = t;
+
+		return alpha;
+
+	}
+
+
+	int min_val(int pa) {
+		int beta = INF;
+		int alpha = pa;
+
+		if (deep == 0) {
+			return eval();
+		}
+
+		int [] step = new int[2]{startpos, endpos};
+
+		int t;
+
+		for (int i : step) {
+
+
+			t = eval();
+
+			if (t < beta) {
+				beta = t;
+			}
+
+
+			if (alpha >= beta) {
+				break;
+			}
+
+		}
+
+
+		beta = t;
+
+		return beta;
+
+	}
