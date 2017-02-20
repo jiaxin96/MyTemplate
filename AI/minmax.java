d80205663c2476e9934ad55f1d5917203afb1184
jiaxin96 <572673807@qq.com>
2017-02-20 15:55:27 +0800

    minmax search

--- AI/minmax.java
+++ AI/minmax.java
@@ -1,71 +1,110 @@
+    int max_val(int[] nums , int pb, int startpos, int endpos, int dp) {
+        int alpha = -INF;
+        int beta = pb;
 
-	int max_val(int pb) {
-		int alpha = -INF;
-		int beta = pb;
+        if (startpos == endpos) {
 
-		if (deep == 0) {
-			return eval();
-		}
+            wins =  (maxp+startpos > minp);
 
-		int [] step = new int[2]{startpos, endpos};
+            if (dp == 0) {
+                value_g = maxp + nums[startpos] - minp;
+            }
 
-		int t;
+            return maxp + nums[startpos] - minp;
+        }
 
-		for (int i : step) {
+        int [] step = new int[] {startpos, endpos};
 
+        int t = -INF;
 
-			t = min_val();
-
-			if (t > alpha) {
-				alpha = t;
-			}
-
-
-			if (alpha >= beta) {
-				break;
-			}
-
-		}
+        for (int i : step) {
 
 
-		alpha = t;
+            // set( maxp, i);
+            maxp += nums[i];
 
-		return alpha;
+            if (i == startpos) {
+                t = min_val(nums, alpha, startpos + 1, endpos, dp+1);
+            }
+            else {
+                t = min_val(nums, alpha, startpos, endpos - 1, dp+1);
+            }
 
-	}
-
-
-	int min_val(int pa) {
-		int beta = INF;
-		int alpha = pa;
-
-		if (deep == 0) {
-			return eval();
-		}
-
-		int [] step = new int[2]{startpos, endpos};
-
-		int t;
-
-		for (int i : step) {
+            // rollback( maxp, i);
+            maxp -= nums[i];
 
 
-			t = max_val();
+            if (value_g > 0) {
+                alpha = INF;
+                break;
+            }
 
-			if (t < beta) {
-				beta = t;
-			}
-
-
-			if (alpha >= beta) {
-				break;
-			}
-
-		}
+            if (t > alpha) {
+                alpha = t;
+            }
 
 
-		beta = t;
+            if (alpha >= beta) {
+                break;
+            }
 
-		return beta;
+        }
 
-	}
+
+
+        return alpha;
+
+    }
+
+
+    int min_val(int[] nums, int pa, int startpos, int endpos, int dp) {
+        int beta = INF;
+        int alpha = pa;
+
+        if (startpos == endpos) {
+
+            wins =  (maxp > minp+startpos);
+
+            return maxp - nums[startpos] - minp;
+        }
+
+        int [] step = new int[]{startpos, endpos};
+
+        int t= INF;
+
+        for (int i : step) {
+
+            // set( minp, i);
+            minp += nums[i];
+
+
+            if (i == startpos) {
+                t = max_val(nums, beta, startpos + 1, endpos, dp+1);
+            }
+            else {
+                t = max_val(nums, beta, startpos, endpos - 1, dp+1);
+            }
+
+
+            // rollback(minp, i);
+            minp -= nums[i];
+
+            if (value_g < 0) {
+                alpha = -INF;
+                break;
+            }
+
+            if (t < beta) {
+                beta = t;
+            }
+
+            if (alpha >= beta) {
+                break;
+            }
+
+        }
+
+
+        return beta;
+
+    }
